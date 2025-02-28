43f03d0cc19e5f534a02757151b770bf91116361
Astra <xphysics@protonmail.ch>
2025-02-28 15:17:46 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from '@modules/users/users.module';
 import { ApiLoggerMiddleware } from '@common/middleware/api-logger.middleware';
 import { ConfigModule } from '@nestjs/config';
@@ -9,7 +8,7 @@ import { User } from '@models/user.model';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({cache: true, isGlobal: true}),
+    ConfigModule.forRoot({ cache: true, isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       url: process.env.DB_URL,
@@ -17,14 +16,13 @@ import { User } from '@models/user.model';
       autoLoadEntities: true,
       entities: [User],
     }),
-    UsersModule],
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(ApiLoggerMiddleware)
-      .forRoutes('*');
+    consumer.apply(ApiLoggerMiddleware).forRoutes('*');
   }
 }
